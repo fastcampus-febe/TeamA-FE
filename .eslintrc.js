ae77612560d21c06139465e92bfed17a1169db38
hwanky <48482406+hwanky@users.noreply.github.com>
2023-01-25 16:35:28 +0900

    Add initial settings

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  env: {
+      browser: true,
+      es6: true,
+  },
+  extends: [ 
+    "react-app", 
+    "plugin:prettier/recommended"
+  ],
+  rules: {
+  }
+};
