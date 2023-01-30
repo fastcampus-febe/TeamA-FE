1d7edf501c96efb232ed013b2a4eb324623dbe5c
plou102 <plou102@naver.com>
2023-01-30 21:53:23 +0900

    update review page

--- src/components/Mypage/ReviewList/ReviewList.jsx
+++ src/components/Mypage/ReviewList/ReviewList.jsx
@@ -1,7 +1,57 @@
 import React from 'react';
+import styled from 'styled-components';
+import { BiLike } from 'react-icons/bi';
 
-const ReviewList = () => {
-  return <div>ReviewList</div>;
+const ReviewList = ({ data }) => {
+  const indate = data.indate.substr(0, 10);
+  return (
+    <ReviewLi>
+      <h3>{data.place_title}</h3>
+      <GoodContent>
+        <BiLike size="17" />
+        <span>
+          {data.good} | {indate}
+        </span>
+      </GoodContent>
+      <p>{data.review}</p>
+    </ReviewLi>
+  );
 };
+
+const ReviewLi = styled.li`
+  margin: 15px 5px;
+  padding: 10px 5px 5px;
+  border-bottom: 1px solid #eaeaea;
+  min-height: 110px;
+  h3 {
+    margin-bottom: 5px;
+    font-size: 18px;
+    line-height: 1.4;
+    font-weight: 600;
+  }
+  p {
+    margin-top: 10px;
+    color: #606060;
+    font-size: 17px;
+  }
+`;
+
+const GoodContent = styled.div`
+  position: relative;
+  display: table;
+  height: 20px;
+  svg {
+    color: #ff385c;
+    position: absolute;
+    left: 0;
+    top: 0;
+  }
+  span {
+    padding-left: 20px;
+    color: #606060;
+    display: table-cell;
+    vertical-align: middle;
+  }
+`;
 
 export default ReviewList;
--- src/pages/Mypage/MyReview/MyReview.jsx
+++ src/pages/Mypage/MyReview/MyReview.jsx
@@ -1,13 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { PageContent, Title } from '../MyPageStyle';
+import reviewData from 'data/MyReviewData.json';
+import styled from 'styled-components';
+import ReviewList from 'components/Mypage/ReviewList/ReviewList';
+import Pagination from 'components/common/Pagination';
 
 const MyReview = () => {
+  const [review, setReview] = useState([]);
+  const [pageDisplay, setPageDisplay] = useState(true);
+  const [page, setPage] = useState(1);
+  const limit = 4;
+  const offset = (page - 1) * limit;
+
+  useEffect(() => {
+    async function getReviewData() {
+      try {
+        setReview(reviewData.items);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getReviewData();
+  }, []);
+
+  console.log(review);
+
   return (
     <PageContent>
-      <Title>리뷰 관리</Title>
-      {/* <LikeList /> */}
+      <Title>리뷰 관리(데이터 임시로 적용) - 상세주소 완료되면 링크 연결하기(관광지 이름)</Title>
+      <ReviewContent>
+        {review.length > 0 ? (
+          review.slice(offset, offset + limit).map((item) => {
+            return <ReviewList data={item} key={item.place_id} />;
+          })
+        ) : (
+          <p>조회 내역이 없습니다.</p>
+        )}
+      </ReviewContent>
+      <PageDisplay pageDisplay={pageDisplay}>
+        {review ? (
+          <Pagination total={review.length} limit={limit} page={page} setPage={setPage} />
+        ) : null}
+      </PageDisplay>
     </PageContent>
   );
 };
+
+const ReviewContent = styled.ul``;
+
+const PageDisplay = styled.div`
+  margin-top: 20px;
+  display: ${(props) => (props.pageDisply ? 'none' : 'block')};
+`;
 
 export default MyReview;
