bba6e56feb22fcfaa985bb2fdbf90d354122933e
plou102 <plou102@naver.com>
2023-01-30 16:21:26 +0900

    create MyLike page

--- src/pages/Mypage/MyLike/MyLike.jsx
+++ src/pages/Mypage/MyLike/MyLike.jsx
@@ -1,14 +1,59 @@
 import LikeList from 'components/Mypage/LikeList/LikeList';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { PageContent, Title } from '../MyPageStyle';
+import likeData from 'data/MyLikeData.json';
+import styled from 'styled-components';
+import Pagination from 'components/common/Pagination';
 
 const MyLike = () => {
+  const [like, setLike] = useState([]);
+  const [pageDisplay, setPageDisplay] = useState(true);
+  const [page, setPage] = useState(1);
+  const limit = 4;
+  const offset = (page - 1) * limit;
+
+  useEffect(() => {
+    async function getLikeData() {
+      try {
+        setLike(likeData.items);
+        setPageDisplay(false);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getLikeData();
+  }, []);
+
   return (
     <PageContent>
       <Title>위시리스트</Title>
-      {/* <LikeList /> */}
+      <LikeContent>
+        {like.length > 0 ? (
+          like.slice(offset, offset + limit).map((item) => {
+            return <LikeList data={item} key={item.id} />;
+          })
+        ) : (
+          <p>조회 내역이 없습니다.</p>
+        )}
+      </LikeContent>
+      <PageDisplay pageDisplay>
+        {like ? (
+          <Pagination total={like.length} limit={limit} page={page} setPage={setPage} />
+        ) : null}
+      </PageDisplay>
     </PageContent>
   );
 };
+
+const LikeContent = styled.ul`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-around;
+`;
+
+const PageDisplay = styled.div`
+  margin-top: 20px;
+  display: ${(props) => (props.pageDisply ? 'none' : 'block')};
+`;
 
 export default MyLike;
