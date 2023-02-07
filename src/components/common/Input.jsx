ec24f038ce5ff7dce1b87ba03531c649345523a0
autumnly1007 <autumnly1007@gmail.com>
2023-02-07 18:20:27 +0900

    Update common components

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -13,6 +13,8 @@ const Input = ({
   padding,
   radius,
   borderColor,
+  fontSize,
+  value,
 }) => {
   return (
     <StyledInput
@@ -27,21 +29,26 @@ const Input = ({
       height={height}
       radius={radius}
       borderColor={borderColor}
+      fontSize={fontSize}
+      defaultValue={value}
     />
   );
 };
 
 const StyledInput = styled.input`
-  border: 1px solid ${({ borderColor }) => (borderColor ? borderColor : '#b0b0b0')};
+  border: 1px solid ${({ borderColor }) => (borderColor ? borderColor : '#dddddd')};
   border-radius: ${({ radius }) => (radius ? radius : '0.4rem')};
   width: ${({ width }) => (width ? width : 'auto')};
   height: ${({ height }) => (height ? height : 'auto')};
   padding: ${({ padding }) => (padding ? padding : '1rem')};
   box-sizing: border-box;
   outline: none;
-  font-size: ${({ theme }) => theme.fonts.size.xsm};
+  font-size: ${({ fontSize, theme }) => (fontSize ? fontSize : theme.fonts.size.xsm)};
   line-height: 1.2rem;
   accent-color: ${({ theme }) => theme.colors.primary};
+  &:focus {
+    border: 1px solid #7a7a7a;
+  }
 `;
 
 export default Input;
