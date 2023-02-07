18a6ebe899c7471059b2c5ad66fdc927773d211c
hwanky <48482406+hwanky@users.noreply.github.com>
2023-02-07 19:38:16 +0900

    Update detail page

--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -1,12 +1,33 @@
 import PlaceDetail from 'components/Detail/PlaceDetail';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import searchData from 'data/SearchData.json';
+import { useLocation } from 'react-router-dom';
 
 const Detail = () => {
+  const [detail, setDetail] = useState([]);
+  const location = useLocation();
+
+  useEffect(() => {
+    async function getDetailData() {
+      try {
+        setDetail(searchData.items);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getDetailData();
+  }, [detail]);
+
   return (
     <DetailContainer>
-      디테일
-      <PlaceDetail></PlaceDetail>
+      <>
+        {detail
+          .filter((item) => item.id === location.pathname.split('/').slice(-1).join(''))
+          .map((item) => {
+            return <PlaceDetail data={item} key={item.id} />;
+          })}
+      </>
     </DetailContainer>
   );
 };
