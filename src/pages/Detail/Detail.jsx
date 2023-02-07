182091330c14de1985eb8dff73f3cb80c29cd2ab
hwanky <48482406+hwanky@users.noreply.github.com>
2023-02-07 14:20:30 +0900

    Add detail page 파일만 추가

--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -1,8 +1,14 @@
+import PlaceDetail from 'components/Detail/PlaceDetail';
 import React from 'react';
 import styled from 'styled-components';
 
 const Detail = () => {
-  return <DetailContainer>상세</DetailContainer>;
+  return (
+    <DetailContainer>
+      디테일
+      <PlaceDetail></PlaceDetail>
+    </DetailContainer>
+  );
 };
 
 const DetailContainer = styled.div``;
